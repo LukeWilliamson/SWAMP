f309f200cf4eec35e4d8191be55bc31a4ad903db
Noah-Mariotti <noahangelom2002@gmail.com>
2020-01-16 15:37:17 -0800

    pause menu
    resume quit and exit to menu

--- SWAMP Team Project/Assets/Scripts/PauseMenuUI.cs
+++ SWAMP Team Project/Assets/Scripts/PauseMenuUI.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject pauseMenuUI;
+    public static bool gameIsPaused = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("TestScene");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
