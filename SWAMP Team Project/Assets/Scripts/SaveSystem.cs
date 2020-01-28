af41ff1baf59e5eaf7832d0d0b311e462ca84974
Noah-Mariotti <noahangelom2002@gmail.com>
2020-01-28 10:04:03 -0800

    save system

---
+++ SWAMP Team Project/Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+public static class SaveSystem
+{
+    public static void savePlayer(PlayerController player)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/player.png";
+
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData data = new PlayerData(player);
+
+        formatter.Serialize(stream, data);
+        Stream.Close();
+    }
+}
