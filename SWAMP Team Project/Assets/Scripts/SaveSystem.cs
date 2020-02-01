6ff7e7e93116f7687de14ceb3a03e5fae9594c29
Noah-Mariotti <noahangelom2002@gmail.com>
2020-02-01 13:15:10 -0800

    pause menu

--- SWAMP Team Project/Assets/Scripts/SaveSystem.cs
+++ SWAMP Team Project/Assets/Scripts/SaveSystem.cs
@@ -5,16 +5,39 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 public static class SaveSystem
 {
-    public static void savePlayer(PlayerController player)
+    public static void SavePlayer(PlayerController player)
     {
         BinaryFormatter formatter = new BinaryFormatter();
-        string path = Application.persistentDataPath + "/player.png";
+        string path = Application.persistentDataPath + "/player.pg";
 
         FileStream stream = new FileStream(path, FileMode.Create);
 
         PlayerData data = new PlayerData(player);
 
         formatter.Serialize(stream, data);
-        Stream.Close();
+
+        stream.Close();
+    }
+
+    public static PlayerData LoadPlayer()
+    {
+        string path = Application.persistentDataPath + "/player.pg";
+
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+
+            return data;
+
+        } else
+        {
+            Debug.Log("Save file not found is " + path);
+            return null;
+
+        }
     }
 }
